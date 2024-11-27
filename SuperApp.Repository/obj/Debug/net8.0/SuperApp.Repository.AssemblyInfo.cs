a94ab9b819fce24f3d5e0a73b9395bf054f89b5e
rafael.ponciano <rafael.ponciano@hotmail.com>
2024-11-27 14:50:24 -0300

    finalizando api para entrega

--- SuperApp.Repository/obj/Debug/net8.0/SuperApp.Repository.AssemblyInfo.cs
+++ SuperApp.Repository/obj/Debug/net8.0/SuperApp.Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17fd1570e9d8a11c8e0faa3af57c2463c687459")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8666c65e3ecc8936189efb0d875cc65ec112a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
