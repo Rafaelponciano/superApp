3c29ff899513e7713666ade3b7ca7d908b162563
rafael.ponciano <rafael.ponciano@hotmail.com>
2024-11-27 15:05:33 -0300

    inserindo ddls

--- SuperApp.Repository/obj/Debug/net8.0/SuperApp.Repository.AssemblyInfo.cs
+++ SuperApp.Repository/obj/Debug/net8.0/SuperApp.Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8666c65e3ecc8936189efb0d875cc65ec112a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94ab9b819fce24f3d5e0a73b9395bf054f89b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperApp.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
